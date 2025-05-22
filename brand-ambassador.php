75a3f5634d58bcd9d22a2291af3801b9215bc3ec
Alexandra <109337175+avs-alexandra@users.noreply.github.com>
2025-05-23 00:49:32 +0700

    Update brand-ambassador.php

--- brand-ambassador.php
+++ brand-ambassador.php
@@ -3,7 +3,7 @@
 Plugin Name: Brand Ambassador
 Plugin URI: https://github.com/avs-alexandra/brand-ambassador
 Description: Плагин для управления амбассадорами бренда и их вознаграждениями.
-Version: 1.0.0
+Version: 1.0.4
 Author: Ваша Имя
 Author URI: https://github.com/avs-alexandra
 Text Domain: brand-ambassador
